c5a4c800631eb2c9bfb5155a016ea96d58e99975
trivediheena <hntrivedi72@gmail.com>
2023-05-27 18:57:28 +0530

    Created Public Subnet

--- create_vpc.sh
+++ create_vpc.sh
@@ -7,4 +7,11 @@ read -p "Enter VPC Name " vpc_name
 
 $VPC_CREATED=$(aws ec2 create-vpc --cidr-block 10.0.0.0/16 --tag-specification ResourceType=vpc,Tags=[{Key=Name,Value=${vpc_name}}])
 
-echo " VPC Created '$VPC_CREATED' in the AWS Region '$region'"
+logger -i " VPC Created '$VPC_CREATED' in the AWS Region '$region'"
+
+read -p "Enter availability zone to create subnet " subnet_public
+# read -p "Enter the name of public subnet " subnet_public_name
+
+$SUBNET_PUBLIC=$(aws ec2 create-subnet --vpc-id $VPC_CREATED --cidr-block 10.0.1.0/24 --availability-zone $subnet_public --region $region)
+
+logger -i "Public Subnet Created '$SUBNET_PUBLIC'"
